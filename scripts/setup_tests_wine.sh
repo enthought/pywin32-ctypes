f9040f1bd2728f947c504c28458c2bf0e00f01ea
David Cournapeau <cournape@gmail.com>
2014-02-12 21:03:49 +0000

    MAINT: install test dependencies.

--- scripts/setup_tests_wine.sh
+++ scripts/setup_tests_wine.sh
@@ -1,13 +1,15 @@
 #! /bin/sh
 set -e
 
+export DISPLAY=":99.0"
+
 PYTHON="c:/Python27/python.exe"
 EASY_INSTALL="c:/Python27/Scripts/easy_install.exe"
 PIP="c:/Python27/Scripts/pip.exe"
 
 wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py
 wget http://www.python.org/ftp/python/2.7.6/python-2.7.6.msi
-wine msiexec /i python-2.7.6.msi
+wine msiexec /i python-2.7.6.msi /qn
 
 wine ${PYTHON} ez_setup.py
 wine ${EASY_INSTALL} pip
