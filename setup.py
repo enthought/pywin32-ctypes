7b266889b2637bf7f735295126b6e6b978dbcc9b
Ioannis Tziakos <ioannist@enthought.com>
2014-02-12 15:48:06 +0000

    MAINT: first setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='mini_pywin32',
+    version='0.0.1.dev1',
+    author='Enthought Inc',
+    author_email='info@enthought.com',
+    packages=['mini_pywin32', 'mini_pywin32.tests'])
