8bd2d02b21d325457ed58523d93ec7063b4d00a8
Ioannis Tziakos <ioannist@enthought.com>
2014-02-12 21:07:47 +0000

    more cleanup

--- mini_pywin32/_win32api.py
+++ mini_pywin32/_win32api.py
@@ -2,32 +2,67 @@ from __future__ import absolute_import
 
 import ctypes
 from ctypes.wintypes import (
-    BOOL, DWORD, HANDLE, HMODULE, LONG, LPCWSTR, LPARAM, WCHAR, WORD)
+    BOOL, DWORD, HANDLE, HMODULE, LONG, LPCWSTR, LPARAM, WCHAR, WORD,
+    HRSRC, HGLOBAL, LPVOID)
 
-from .util import check_null, check_zero
+from .util import check_null, check_zero, function_factory, LONG_PTR
 
 kernel32 = ctypes.windll.kernel32
 
-_LoadLibraryEx = kernel32.LoadLibraryExW
-_LoadLibraryEx.argtypes = [LPCWSTR, HANDLE, DWORD]
-_LoadLibraryEx.restype = HMODULE
-_LoadLibraryEx.errcheck = check_null
 
-_FreeLibrary = kernel32.FreeLibrary
-_FreeLibrary.errcheck = check_zero
+_LoadLibraryEx = function_factory(
+        kernel32.LoadLibraryExW,
+        [LPCWSTR, HANDLE, DWORD],
+        HMODULE, check_null)
 
-_EnumResTypeProc = ctypes.WINFUNCTYPE(BOOL, HMODULE, LONG, LPARAM)
-_EnumResNameProc = ctypes.WINFUNCTYPE(BOOL, HMODULE, LONG, LONG, LPARAM)
-_EnumResLanguagesProc = ctypes.WINFUNCTYPE(
+_FreeLibrary = function_factory(
+    kernel32.FreeLibrary,
+    [HMODULE],
+    BOOL,
+    check_zero)
+
+ENUMRESTYPEPROC = ctypes.WINFUNCTYPE(BOOL, HMODULE, LONG, LPARAM)
+_EnumResourceTypes = function_factory(
+    kernel32.EnumResourceTypesW,
+    [HMODULE, ENUMRESTYPEPROC, LONG_PTR],
+    BOOL,
+    check_zero)
+
+ENUMRESNAMEPROC = ctypes.WINFUNCTYPE(BOOL, HMODULE, LONG, LONG, LPARAM)
+_EnumResourceNames = function_factory(
+    kernel32.EnumResourceNamesW,
+    [HMODULE, DWORD, ENUMRESNAMEPROC, LONG_PTR],
+    BOOL,
+    check_zero)
+
+ENUMRESLANGPROC = ctypes.WINFUNCTYPE(
     BOOL, HMODULE, WCHAR, WCHAR, WORD, LPARAM)
+_EnumResourceLanguages = function_factory(
+    kernel32.EnumResourceLanguagesW,
+    [HMODULE, LPCWSTR, LPCWSTR, ENUMRESLANGPROC, LONG_PTR],
+    BOOL,
+    check_zero)
 
-_EnumResourceTypes = kernel32.EnumResourceTypesW
-_EnumResourceNames = kernel32.EnumResourceNamesW
-_EnumResourceLanguages = kernel32.EnumResourceLanguagesW
+_LoadResource = function_factory(
+    kernel32.LoadResource,
+    [HMODULE, HRSRC],
+    HGLOBAL,
+    check_zero)
 
-_LoadResource = kernel32.LoadResource
-_LoadResource.errcheck = check_zero
+_LockResource = function_factory(
+    kernel32.LockResource,
+    [HGLOBAL],
+    LPVOID,
+    check_null)
 
-_LockResource = kernel32.LockResource
-_FindResourceEx = kernel32.FindResourceExW
-_SizeofResource = kernel32.SizeofResource
+_FindResourceEx = function_factory(
+    kernel32.FindResourceExW,
+    [HMODULE, DWORD, DWORD, WORD],
+    HRSRC,
+    check_null)
+
+_SizeofResource = function_factory(
+    kernel32.SizeofResource,
+    [HMODULE, HRSRC],
+    DWORD,
+    check_zero)
--- mini_pywin32/win32api.py
+++ mini_pywin32/win32api.py
@@ -10,69 +10,44 @@ LOAD_LIBRARY_AS_DATAFILE = 0x2
 def LoadLibraryEx(FileName, handle, flags):
     if not handle == 0:
         raise ValueError("handle != 0 not supported")
-    c_filename = _win32api.LPCWSTR(FileName)
-    c_flags = flags
-
-    return _win32api._LoadLibraryEx(c_filename, 0, c_flags)
+    return _win32api._LoadLibraryEx(FileName, 0, flags)
 
 
 def EnumResourceTypes(hModule):
     resource_types = []
-
     def callback(hModule, typeid, lParam):
         resource_types.append(typeid)
         return True
     _win32api._EnumResourceTypes(
-        hModule, _win32api._EnumResTypeProc(callback), None)
+        hModule, _win32api.ENUMRESTYPEPROC(callback), 0)
     return resource_types
 
 
 def EnumResourceNames(hModule, Type):
-    c_hmodule = _win32api.HMODULE(hModule)
-    c_type = Type
-
     resource_names = []
-
     def callback(hModule, type_id, type_name, lParam):
         resource_names.append(type_name)
         return True
     _win32api._EnumResourceNames(
-        c_hmodule, c_type, _win32api._EnumResNameProc(callback), None)
+        hModule, Type, _win32api.ENUMRESNAMEPROC(callback), 0)
     return resource_names
 
 
 def EnumResourceLanguages(hModule, Type, Name):
-    c_hmodule = _win32api.HMODULE(hModule)
-    c_type = Type
-    c_name = Name
-
     resource_languages = []
-
     def callback(hModule, type_name, res_name, language_id, lParam):
         resource_languages.append(language_id)
         return True
-    _win32api._EnumResourceLanguages(
-        c_hmodule, c_type, c_name,
-        _win32api._EnumResLanguagesProc(callback), None)
+    _win32api._EnumResourceLanguages(hModule, Type, Name, callback, None)
     return resource_languages
 
 
 def LoadResource(hModule, type_, name, language):
-    c_hModule = _win32api.HMODULE(hModule)
-    c_type = type_
-    c_name = name
-    c_language = WORD(language)
-
-    c_hrsrc = _win32api._FindResourceEx(c_hModule, c_type, c_name, c_language)
-
-    assert c_hrsrc is not None, "Error handling missing"
-    c_size = _win32api._SizeofResource(c_hModule, c_hrsrc)
-    assert c_size != 0, "Error handling missing"
-    c_hglob = _win32api._LoadResource(c_hModule, c_hrsrc)
-    assert c_hglob is not None, "Error handling missing"
-    c_pointer = _win32api._LockResource(c_hglob)
-    assert c_hglob is not None, "Error handling missing"
-    return _win32cred._PyString_FromStringAndSize(c_pointer, c_size)
+    hrsrc = _win32api._FindResourceEx(hModule, type_, name, language)
+    size = _win32api._SizeofResource(hModule, hrsrc)
+    hglob = _win32api._LoadResource(hModule, hrsrc)
+    pointer = _win32api._LockResource(hglob)
+    return _win32cred._PyString_FromStringAndSize(pointer, size)
 
 
 def FreeLibrary(hModule):
