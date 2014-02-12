2ae2a3bee502f1eab8129adf49d6903bcb22670d
David Cournapeau <cournape@gmail.com>
2014-02-12 21:49:36 +0000

    MAINT: install pywin32.

--- scripts/setup_tests_wine.sh
+++ scripts/setup_tests_wine.sh
@@ -15,3 +15,7 @@ tar xf setuptools-2.2.tar.gz
 (cd setuptools-2.2 && wine ${PYTHON} setup.py install)
 
 wine ${EASY_INSTALL} nose coverage
+
+wget http://sourceforge.net/projects/pywin32/files/pywin32/Build%20218/pywin32-218.win32-py2.7.exe/download -O pywin32-218.win32-py2.7.exe
+
+wine ${EASY_INSTALL} pywin32-218.win32-py2.7.exe
