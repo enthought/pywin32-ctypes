2c858c8a6d0b5d7f4d2271558fc5d973aab112c4
David Cournapeau <cournape@gmail.com>
2014-02-13 20:09:33 +0000

    MAINT: fix travis-ci script after rename.

--- scripts/run_tests_wine.sh
+++ scripts/run_tests_wine.sh
@@ -6,7 +6,7 @@ export DISPLAY=:99.0
 PYTHON="c:/Python27/python.exe"
 COVERAGE="c:/Python27/Scripts/coverage.exe"
 
-wine ${PYTHON} -m nose.core wine32ctypes
+wine ${PYTHON} -m nose.core win32ctypes
 wine ${COVERAGE} erase
-wine ${COVERAGE} run -m nose.core wine32ctypes
+wine ${COVERAGE} run -m nose.core win32ctypes
 wine ${COVERAGE} report --include=win32ctypes*
