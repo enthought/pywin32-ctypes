2ff50652101096e240a72fa156dbc1cb6319bbdb
Ioannis Tziakos <ioannist@enthought.com>
2014-02-25 10:28:27 +0000

    update travis-ci configuration to setup runs for python 2.6

--- scripts/run_tests_wine.sh
+++ scripts/run_tests_wine.sh
@@ -5,6 +5,8 @@ export DISPLAY=:99.0
 
 if [ "${TRAVIS_PYTHON_VERSION}" = "2.7" ]; then
     COVERAGE="c:/Python27/Scripts/coverage.exe"
+elif [ "${TRAVIS_PYTHON_VERSION}" = "2.6" ]; then
+    COVERAGE="c:/Python26/Scripts/coverage.exe"
 elif [ "${TRAVIS_PYTHON_VERSION}" = "3.2" ]; then
     COVERAGE="c:/Python32/Scripts/coverage.exe"
 elif [ "${TRAVIS_PYTHON_VERSION}" = "3.3" ]; then
