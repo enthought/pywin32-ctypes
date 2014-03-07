b7cb763a64941380fe2cd7247a3b5f8054d0d7c7
David Cournapeau <cournape@gmail.com>
2014-03-07 14:15:55 +0000

    MAINT: release 0.0.1.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pywin32-ctypes',
-    version='0.0.1.dev1',
+    version='0.0.1',
     author='Enthought Inc',
     author_email='info@enthought.com',
     packages=['win32ctypes', 'win32ctypes.tests'],
