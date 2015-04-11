dbb3029431cb7a0f194da54b820b4b391c8e8bfb
Ioannis Tziakos <ioannist@enthought.com>
2015-04-11 20:23:18 +0100

    use 2to3 to support python 3.2

--- setup.py
+++ setup.py
@@ -12,4 +12,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
-    ])
+    ],
+    use_2to3=True,
+    zip_safe=False,
+)
