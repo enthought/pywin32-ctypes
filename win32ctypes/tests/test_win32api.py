af15378b9d2cf6d5845b031a349e8b46d4aef18a
Ioannis Tziakos <ioannist@enthought.com>
2015-04-11 02:48:47 +0100

    cleanup tests

--- win32ctypes/tests/test_win32api.py
+++ win32ctypes/tests/test_win32api.py
@@ -8,6 +8,7 @@
 
 import sys
 import unittest
+import contextlib
 
 import win32api
 from win32ctypes import pywin32
@@ -17,122 +18,93 @@ from win32ctypes.tests import compat
 
 class TestWin32API(compat.TestCase):
 
-    def setUp(self):
-        self.handle = None
+    module = pywin32.win32api
 
-    def tearDown(self):
-        if self.handle is not None:
-            self._free_library(win32api, self.handle)
+    @contextlib.contextmanager
+    def load_library(self, module, library=sys.executable, flags=0x2):
+        handle = module.LoadLibraryEx(library, 0, flags)
+        try:
+            yield handle
+        finally:
+            module.FreeLibrary(handle)
 
     def test_load_library_ex(self):
-        self.handle = self._load_library(win32api)
-        mini = self._load_library(pywin32.win32api)
-        self.assertEqual(mini, self.handle)
+        with self.load_library(win32api) as expected:
+            with self.load_library(self.module) as handle:
+                self.assertEqual(handle, expected)
 
         with self.assertRaises(error):
-            pywin32.win32api.LoadLibraryEx('ttt.dll', 0, 0x2)
+            self.module.LoadLibraryEx('ttt.dll', 0, 0x2)
 
     def test_free_library(self):
-        self.handle = self._load_library(win32api)
-        self.assertTrue(self._free_library(win32api, self.handle) is None)
-        self.assertNotEqual(
-            self._free_library(pywin32.win32api, self.handle), 0)
+        with self.load_library(win32api) as handle:
+            self.assertTrue(win32api.FreeLibrary(handle) is None)
+            self.assertNotEqual(self.module.FreeLibrary(handle), 0)
 
         with self.assertRaises(error):
-            self._free_library(pywin32.win32api, -3)
+            self.module.FreeLibrary(-3)
 
     def test_enum_resource_types(self):
-        self.handle = self._load_library(win32api, 'explorer.exe')
-        original = self._enum_resource_types(win32api, self.handle)
-        mini = self._enum_resource_types(pywin32.win32api, self.handle)
-        self.assertEqual(mini, original)
+        with self.load_library(win32api, 'explorer.exe') as handle:
+            expected = win32api.EnumResourceTypes(handle)
+
+        with self.load_library(pywin32.win32api, 'explorer.exe') as handle:
+            resource_types = pywin32.win32api.EnumResourceTypes(handle)
+
+        self.assertEqual(resource_types, expected)
 
         with self.assertRaises(error):
-            pywin32.win32api.EnumResourceTypes(-3)
+            self.module.EnumResourceTypes(-3)
 
     def test_enum_resource_names(self):
-        self.handle = self._load_library(win32api)
-        resource_types = self._enum_resource_types(win32api, self.handle)
-
-        for resource_type in resource_types:
-            original = self._enum_resource_names(
-                win32api, self.handle, resource_type)
-            mini = self._enum_resource_names(
-                pywin32.win32api, self.handle, resource_type)
-            self.assertEqual(mini, original)
+        with self.load_library(win32api, 'explorer.exe') as handle:
+            resource_types = win32api.EnumResourceTypes(handle)
+            for resource_type in resource_types:
+                expected = win32api.EnumResourceNames(handle, resource_type)
+                resource_names = self.module.EnumResourceNames(
+                    handle, resource_type)
+            self.assertEqual(resource_names, expected)
 
         with self.assertRaises(error):
-            pywin32.win32api.EnumResourceNames(2, 3)
+            self.module.EnumResourceNames(2, 3)
 
     def test_enum_resource_languages(self):
-        handle = self._load_library(win32api)
-        resource_types = self._enum_resource_types(win32api, handle)
-
-        for resource_type in resource_types:
-            resource_names = self._enum_resource_names(
-                win32api, handle, resource_type)
-            for resource_name in resource_names:
-                original = self._enum_resource_languages(
-                    win32api, handle, resource_type, resource_name)
-                mini = self._enum_resource_languages(
-                    pywin32.win32api, handle, resource_type,
-                    resource_name)
-                self.assertEqual(mini, original)
+        with self.load_library(win32api, 'explorer.exe') as handle:
+            resource_types = win32api.EnumResourceTypes(handle)
+            for resource_type in resource_types:
+                resource_names = win32api.EnumResourceNames(
+                    handle, resource_type)
+                for resource_name in resource_names:
+                    expected = win32api.EnumResourceLanguages(
+                        handle, resource_type, resource_name)
+                    resource_languages = self.module.EnumResourceLanguages(
+                        handle, resource_type, resource_name)
+                    self.assertEqual(resource_languages, expected)
 
         with self.assertRaises(error):
-            pywin32.win32api.EnumResourceLanguages(
-                handle, resource_type, 2235)
+            self.module.EnumResourceLanguages(handle, resource_type, 2235)
 
     def test_load_resource(self):
-        handle = self._load_library(win32api)
-        resource_types = self._enum_resource_types(win32api, handle)
-
-        for resource_type in resource_types:
-            resource_names = self._enum_resource_names(
-                win32api, handle, resource_type)
-            for resource_name in resource_names:
-                resource_languages = self._enum_resource_languages(
-                    win32api, handle, resource_type, resource_name)
-                for resource_language in resource_languages:
-                    original = self._load_resource(
-                        win32api, handle,
-                        resource_type, resource_name,
-                        resource_language)
-                    mini = self._load_resource(
-                        pywin32.win32api, handle,
-                        resource_type, resource_name,
-                        resource_language)
-                    self.assertEqual(mini, original)
+        with self.load_library(win32api, 'explorer.exe') as handle:
+            resource_types = win32api.EnumResourceTypes(handle)
+            for resource_type in resource_types:
+                resource_names = win32api.EnumResourceNames(
+                    handle, resource_type)
+                for resource_name in resource_names:
+                    resource_languages = win32api.EnumResourceLanguages(
+                        handle, resource_type, resource_name)
+                    for resource_language in resource_languages:
+                        expected = win32api.LoadResource(
+                            handle, resource_type, resource_name,
+                            resource_language)
+                        resource = self.module.LoadResource(
+                            handle, resource_type, resource_name,
+                            resource_language)
+                        self.assertEqual(resource, expected)
 
         with self.assertRaises(error):
-            pywin32.win32api.LoadResource(
+            self.module.LoadResource(
                 handle, resource_type, resource_name, 12435)
-
-    def _load_library(self, module, library=sys.executable):
-        # backward shim for win32api module which does not export
-        # LOAD_LIBRARY_AS_DATAFILE
-        LOAD_LIBRARY_AS_DATAFILE = getattr(
-            module, "LOAD_LIBRARY_AS_DATAFILE", 0x2)
-        return module.LoadLibraryEx(
-            library, 0, LOAD_LIBRARY_AS_DATAFILE)
-
-    def _free_library(self, module, handle):
-        return module.FreeLibrary(handle)
-
-    def _enum_resource_types(self, module, handle):
-        return module.EnumResourceTypes(handle)
-
-    def _enum_resource_names(self, module, handle, resource_type):
-        return module.EnumResourceNames(handle, resource_type)
-
-    def _enum_resource_languages(self, module, handle, resource_type, name):
-        return module.EnumResourceLanguages(handle, resource_type, name)
-
-    def _load_resource(
-            self, module, handle, resource_type,
-            resource_name, resource_language):
-        return module.LoadResource(
-            handle, resource_type, resource_name, resource_language)
 
 
 if __name__ == '__main__':
