e31082622fd11eaef7c128b0683a18cb043e3112
Ioannis Tziakos <ioannist@enthought.com>
2015-04-11 04:02:24 +0100

    better support the two different values for resource type

--- win32ctypes/core/ctypes/_kernel32.py
+++ win32ctypes/core/ctypes/_kernel32.py
@@ -15,11 +15,10 @@ from ctypes.wintypes import (
 from ._common import LONG_PTR
 from ._util import check_null, check_zero, function_factory
 
-# While the lpszType is a LPTSTR we need to treat it as pointer
-_ENUMRESTYPEPROC = ctypes.WINFUNCTYPE(BOOL, HMODULE, LPVOID, LONG_PTR)
 ENUMRESNAMEPROC = ctypes.WINFUNCTYPE(BOOL, HMODULE, LONG, LONG, LONG_PTR)
 ENUMRESLANGPROC = ctypes.WINFUNCTYPE(
     BOOL, HMODULE, WCHAR, WCHAR, WORD, LONG_PTR)
+_ENUMRESTYPEPROC = ctypes.WINFUNCTYPE(BOOL, HMODULE, LPVOID, LONG_PTR)
 
 kernel32 = ctypes.windll.kernel32
 
@@ -53,18 +52,6 @@ _EnumResourceTypes = function_factory(
     BOOL,
     check_zero)
 
-_EnumResourceNames = function_factory(
-    kernel32.EnumResourceNamesW,
-    [HMODULE, DWORD, ENUMRESNAMEPROC, LONG_PTR],
-    BOOL,
-    check_zero)
-
-_EnumResourceLanguages = function_factory(
-    kernel32.EnumResourceLanguagesW,
-    [HMODULE, DWORD, DWORD, ENUMRESLANGPROC, LONG_PTR],
-    BOOL,
-    check_zero)
-
 _LoadResource = function_factory(
     kernel32.LoadResource,
     [HMODULE, HRSRC],
@@ -77,14 +64,46 @@ _LockResource = function_factory(
     LPVOID,
     check_null)
 
-_FindResourceEx = function_factory(
-    kernel32.FindResourceExW,
-    [HMODULE, DWORD, DWORD, WORD],
-    HRSRC,
-    check_null)
-
 _SizeofResource = function_factory(
     kernel32.SizeofResource,
     [HMODULE, HRSRC],
     DWORD,
     check_zero)
+
+_BaseEnumResourceNames = function_factory(
+    kernel32.EnumResourceNamesW,
+    [HMODULE, LPCWSTR, ENUMRESNAMEPROC, LONG_PTR],
+    BOOL,
+    check_zero)
+
+_BaseEnumResourceLanguages = function_factory(
+    kernel32.EnumResourceLanguagesW,
+    [HMODULE, LPCWSTR, LPCWSTR, ENUMRESLANGPROC, LONG_PTR],
+    BOOL,
+    check_zero)
+
+_BaseFindResourceEx = function_factory(
+    kernel32.FindResourceExW,
+    [HMODULE, LPCWSTR, LPCWSTR, WORD],
+    HRSRC,
+    check_null)
+
+
+def _EnumResourceNames(hModule, lpszType, lpEnumFunc, lParam):
+    resource_type = LPCWSTR(lpszType)
+    return _BaseEnumResourceNames(
+        hModule, resource_type, lpEnumFunc, lParam)
+
+
+def _EnumResourceLanguages(hModule, lpType, lpName, lpEnumFunc, lParam):
+    resource_type = LPCWSTR(lpType)
+    resource_name = LPCWSTR(lpName)
+    return _BaseEnumResourceLanguages(
+        hModule, resource_type, resource_name, lpEnumFunc, lParam)
+
+
+def _FindResourceEx(hModule, lpType, lpName, wLanguage):
+    resource_type = LPCWSTR(lpType)
+    resource_name = LPCWSTR(lpName)
+    return _BaseFindResourceEx(
+        hModule, resource_type, resource_name, wLanguage)
