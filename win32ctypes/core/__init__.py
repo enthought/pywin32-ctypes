caaa4758a53bf92caa6c480a7ff8442ce7ecad5b
Ioannis Tziakos <ioannist@enthought.com>
2015-04-13 21:52:40 +0100

    do not keep around the cffi module when import was succeful (it conflics with the cffi module inside win32ctypes

--- win32ctypes/core/__init__.py
+++ win32ctypes/core/__init__.py
@@ -11,10 +11,12 @@ try:
     import cffi  # noqa
 except ImportError:
     from win32ctypes.core.ctypes import _advapi32, _common, _kernel32
+    _backend = 'ctypes'
 else:
     from win32ctypes.core.cffi import _advapi32, _common, _kernel32
+    del cffi
+    _backend = 'cffi'
 
 from win32ctypes.core import _winerrors
-
 
 __all__ = ['_kernel32', '_advapi32', '_winerrors', '_common']
