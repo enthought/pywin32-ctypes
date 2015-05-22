ea86324a489cec3a0273c81d1b56d962c5dda811
Ioannis Tziakos <ioannist@enthought.com>
2015-05-23 00:37:43 +0100

    provide common functions

--- win32ctypes/core/cffi/_common.py
+++ win32ctypes/core/cffi/_common.py
@@ -7,9 +7,21 @@
 #
 from __future__ import absolute_import
 
+from weakref import WeakKeyDictionary
+
 from ._util import ffi
+
+_keep_alive = WeakKeyDictionary()
 
 
 def _PyBytes_FromStringAndSize(pointer, size):
     buffer = ffi.buffer(pointer, size)
     return buffer[:]
+
+
+def byreference(x):
+    return ffi.new(ffi.getctype(ffi.typeof(x), '*'), x)
+
+
+def dereference(x):
+    return x[0]
