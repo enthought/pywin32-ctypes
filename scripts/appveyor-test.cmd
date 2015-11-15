02dc8611433888ff57b0c1178ebc502c3e6dc4ca
Ioannis Tziakos <ioannist@enthought.com>
2015-11-15 17:41:41 +0100

    correct the test runing command

--- scripts/appveyor-test.cmd
+++ scripts/appveyor-test.cmd
@@ -1,6 +1,6 @@
 mkdir testrun
 copy .coveragerc testrun
 cd testrun
-coverage run -m unittest discover -v
+coverage run -m unittest discover -v win32ctypes
 if %errorlevel% neq 0 exit /b %errorlevel%
 coverage report
