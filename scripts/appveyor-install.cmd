385f8f5a22eb21ed6cf8207b4e1aa89cc6c94e5c
Ioannis Tziakos <ioannist@enthought.com>
2015-11-15 17:33:49 +0100

    try with the pywin32 in pypi

--- scripts/appveyor-install.cmd
+++ scripts/appveyor-install.cmd
@@ -3,7 +3,7 @@ call setenv /x64
 
 rem install python packages
 pip install --cache-dir C:/egg_cache coverage
-pip install --cache-dir C:/egg_cache pywin32
+pip install --cache-dir C:/egg_cache --only-binary pypiwin32 pypiwin32
 
 rem install package
 python setup.py develop
