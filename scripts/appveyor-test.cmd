c938b7e2447df80b9ab4e4d5d2a77e0ae0d283fc
Ioannis Tziakos <ioannist@enthought.com>
2015-11-15 18:09:22 +0100

    make sure that we append coverage on the second run

--- scripts/appveyor-test.cmd
+++ scripts/appveyor-test.cmd
@@ -5,6 +5,4 @@ coverage run -m unittest discover -v win32ctypes
 if %errorlevel% neq 0 exit /b %errorlevel%
 pip install --cache-dir C:/egg_cache --upgrade cffi>=1.3.0
 if %errorlevel% neq 0 exit /b %errorlevel%
-coverage run -m unittest discover -v win32ctypes
-if %errorlevel% neq 0 exit /b %errorlevel%
-coverage report
+coverage run -a -m unittest discover -v win32ctypes
