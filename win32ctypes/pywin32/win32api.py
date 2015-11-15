9a73642becaae2a17b94f80537d3683ef8301f6d
Ioannis Tziakos <ioannist@enthought.com>
2015-11-15 13:04:43 +0100

    update docstrings

--- win32ctypes/pywin32/win32api.py
+++ win32ctypes/pywin32/win32api.py
@@ -5,64 +5,184 @@
 # This file is open source software distributed according to the terms in
 # LICENSE.txt
 #
+""" A module, encapsulating the Windows Win32 API. """
 
 from __future__ import absolute_import
 
 from win32ctypes.core import _common, _kernel32
-from win32ctypes.pywin32.pywintypes import pywin32error
+from win32ctypes.pywin32.pywintypes import pywin32error as _pywin32error
 
 LOAD_LIBRARY_AS_DATAFILE = 0x2
+LANG_NEUTRAL = 0x00
 
 
-def LoadLibraryEx(FileName, handle, flags):
+def LoadLibraryEx(fileName, handle, flags):
+    """ Loads the specified DLL, and returns the handle.
+
+    Parameters
+    ----------
+    fileName : unicode
+        The filename of the module to load.
+    handle :
+        Reserved, always zero.
+    flags :
+        The action to be taken when loading the module.
+
+    Returns
+    -------
+    hModule :
+        The handle of the loaded module
+
+    See also
+    --------
+    - `LoadLibraryEx MSDN reference <https://msdn.microsoft.com/en-us/library/windows/desktop/ms684179(v=vs.85).aspx>`_
+
+    """
     if not handle == 0:
         raise ValueError("handle != 0 not supported")
-    with pywin32error():
-        return _kernel32._LoadLibraryEx(FileName, 0, flags)
+    with _pywin32error():
+        return _kernel32._LoadLibraryEx(fileName, 0, flags)
 
 
 def EnumResourceTypes(hModule):
+    """ Enumerates resource types within a module.
+
+    Parameters
+    ----------
+    hModule : handle
+        The handle to the module.
+
+    Returns
+    -------
+    resource_types : list
+       The list of resource types in the module.
+
+    See also
+    --------
+    - `EnumResourceTypes MSDN reference <https://msdn.microsoft.com/en-us/library/windows/desktop/ms648039(v=vs.85).aspx>`_
+
+    """
     resource_types = []
 
     def callback(hModule, type_, param):
         resource_types.append(type_)
         return True
 
-    with pywin32error():
+    with _pywin32error():
         _kernel32._EnumResourceTypes(
             hModule, _kernel32.ENUMRESTYPEPROC(callback), 0)
     return resource_types
 
 
-def EnumResourceNames(hModule, type_):
+def EnumResourceNames(hModule, resType):
+    """ Enumerates all the resources of the specified type within a module.
+
+    Parameters
+    ----------
+    hModule : handle
+        The handle to the module.
+    resType : str : int
+        The type of resource to enumerate. If ``resType`` is a string starting with
+        '#' is should be followed by the decimal number that define the integer
+        resource type identifier.
+
+    Returns
+    -------
+    resource_names : list
+       The list of resource names (unicode strings) of the specific
+       resource type in the module.
+
+    See also
+    --------
+    - `EnumResourceNames MSDN reference <https://msdn.microsoft.com/en-us/library/windows/desktop/ms648037(v=vs.85).aspx>`_
+    - `Predefined resource types <https://msdn.microsoft.com/en-us/library/windows/desktop/ms648009(v=vs.85).aspx>`_
+
+    """
     resource_names = []
 
     def callback(hModule, type_, type_name, param):
         resource_names.append(type_name)
         return True
 
-    with pywin32error():
+    with _pywin32error():
         _kernel32._EnumResourceNames(
-            hModule, type_, _kernel32.ENUMRESNAMEPROC(callback), 0)
+            hModule, resType, _kernel32.ENUMRESNAMEPROC(callback), 0)
     return resource_names
 
 
-def EnumResourceLanguages(hModule, type_, name):
+def EnumResourceLanguages(hModule, lpType, lpName):
+    """ List languages of a resource module.
+
+    Parameters
+    ----------
+    hModule : handle
+        Handle to the resource module.
+    lpType : str : int
+        The type of resource to enumerate. If ``lpType`` is a string starting with
+        '#' is should be followed by the decimal number that define the integer
+        resource type identifier.
+    lpName : str : int
+        The name of resource to enumerate. If ``lpType`` is a string starting with
+        '#' is should be followed by the decimal number that define the integer
+        resource type identifier.
+
+    Returns
+    -------
+    resource_languages : list
+        List of the resource language ids.
+
+
+    See also
+    --------
+    - `EnumResourceLanguages MSDN reference <https://msdn.microsoft.com/en-us/library/windows/desktop/ms648035(v=vs.85).aspx>`_
+    - `Predefined resource types <https://msdn.microsoft.com/en-us/library/windows/desktop/ms648009(v=vs.85).aspx>`_
+    - `Predefined resource language ids <https://msdn.microsoft.com/en-us/library/windows/desktop/dd318693(v=vs.85).aspx>`_
+
+    """
     resource_languages = []
 
     def callback(hModule, type_name, res_name, language_id, param):
         resource_languages.append(language_id)
         return True
 
-    with pywin32error():
+    with _pywin32error():
         _kernel32._EnumResourceLanguages(
-            hModule, type_, name, _kernel32.ENUMRESLANGPROC(callback), 0)
+            hModule, lpType, lpName, _kernel32.ENUMRESLANGPROC(callback), 0)
     return resource_languages
 
 
-def LoadResource(hModule, type_, name, language):
-    with pywin32error():
-        hrsrc = _kernel32._FindResourceEx(hModule, type_, name, language)
+def LoadResource(hModule, type, name, language=LANG_NEUTRAL):
+    """ Find and Load a resource component.
+
+    Parameters
+    ----------
+    handle :
+        The handle of the module containing the resource.
+        Use None for currrent process executable.
+    type : str : int
+        The type of resource to load.
+    name :
+        The name or Id of the resource to load.
+    language : int
+        Language to use, default is LANG_NEUTRAL.
+
+    Returns
+    -------
+    hModule :
+        Handle of the loaded source.
+
+    See also
+    --------
+    - `FindResourceEx MSDN reference <https://msdn.microsoft.com/en-us/library/windows/desktop/ms648043(v=vs.85).aspx>`_
+    - `SizeofResource MSDN reference <https://msdn.microsoft.com/en-us/library/windows/desktop/ms648048(v=vs.85).aspx>`_
+    - `LoadResource MSDN reference <https://msdn.microsoft.com/en-us/library/windows/desktop/ms648046(v=vs.85).aspx>`_
+    - `LockResource MSDN reference <https://msdn.microsoft.com/en-us/library/windows/desktop/ms648047(v=vs.85).aspx>`_
+    - `Predefined resource types <https://msdn.microsoft.com/en-us/library/windows/desktop/ms648009(v=vs.85).aspx>`_
+    - `Predefined resource language ids <https://msdn.microsoft.com/en-us/library/windows/desktop/dd318693(v=vs.85).aspx>`_
+
+    """
+    with _pywin32error():
+        hrsrc = _kernel32._FindResourceEx(hModule, type, name, language)
         size = _kernel32._SizeofResource(hModule, hrsrc)
         hglob = _kernel32._LoadResource(hModule, hrsrc)
         pointer = _kernel32._LockResource(hglob)
@@ -70,5 +190,15 @@ def LoadResource(hModule, type_, name, language):
 
 
 def FreeLibrary(hModule):
-    with pywin32error():
+    """ Free the loaded dynamic-link library (DLL) module.
+
+    If necessary, decrements its reference count.
+
+    Parameters
+    ----------
+    hModule :
+        The handle to the library as returned by the LoadLibrary function.
+
+    """
+    with _pywin32error():
         return _kernel32._FreeLibrary(hModule)
--- win32ctypes/pywin32/win32cred.py
+++ win32ctypes/pywin32/win32cred.py
@@ -5,28 +5,31 @@
 # This file is open source software distributed according to the terms in
 # LICENSE.txt
 #
+""" Interface to credentials management functions. """
+
 from __future__ import absolute_import
 
 from win32ctypes.core import _advapi32, _common
-from win32ctypes.pywin32.pywintypes import pywin32error
+from win32ctypes.pywin32.pywintypes import pywin32error as _pywin32error
 
 CRED_TYPE_GENERIC = 0x1
 CRED_PERSIST_ENTERPRISE = 0x3
+CRED_PRESERVE_CREDENTIAL_BLOB = 0
 
 
-def CredWrite(credential, flag):
-    """
-    Mimic pywin32 win32cred.CredWrite.
+def CredWrite(Credential, Flags=CRED_PRESERVE_CREDENTIAL_BLOB):
+    """ Creates or updates a stored credential.
 
     Parameters
     ----------
-    credential: dict
-        Dict of parameters to be passed to win32 API CredWrite
-    flag: int
+    Credential : dict
+        Parameters to be passed to win32 API CredWrite/
+    Flags : int
+        Always pass CRED_PRESERVE_CREDENTIAL_BLOB (i.e. 0).
 
     Returns
     -------
-    credentials: dict
+    credentials : dict
         A dictionary containing the following:
 
             - Type: the type of credential (see MSDN)
@@ -36,58 +39,64 @@ def CredWrite(credential, flag):
             - CredentialBlob: the password (as a *string*, not an encoded
               binary stream - this function takes care of the encoding).
             - Comment: a string
+
     """
-    c_creds = _advapi32.CREDENTIAL.fromdict(credential, flag)
+    c_creds = _advapi32.CREDENTIAL.fromdict(Credential, Flags)
     c_pcreds = _advapi32.PCREDENTIAL(c_creds)
-    with pywin32error():
+    with _pywin32error():
         _advapi32._CredWrite(c_pcreds, 0)
 
 
-def CredRead(TargetName, Type):
-    """
-    Mimic pywin32 win32cred.CreadRead.
+def CredRead(TargetName, Type, Flags=0):
+    """ Retrieves a stored credential.
 
     Parameters
     ----------
-    TargetName: str-like
+    TargetName : unicode
         The target name to fetch from the keyring.
+    Type : int
+        One of the CRED_TYPE_* constants.
+    Flags : int
+        Reserved, always use 0.
 
     Returns
     -------
-    credentials: dict
+    credentials : dict
         A dictionary containing the following:
 
             - UserName: the retrieved username
             - CredentialBlob: the password (as an utf-16 encoded 'string')
 
-        None if the target name was not found.
+        ``None`` if the target name was not found.
+
     """
     if Type != CRED_TYPE_GENERIC:
         raise ValueError("Type != CRED_TYPE_GENERIC not yet supported")
 
-    flag = 0
     pcreds = _advapi32.PCREDENTIAL()
-    with pywin32error():
+    with _pywin32error():
         _advapi32._CredRead(
-            TargetName, Type, flag, _common.byreference(pcreds))
+            TargetName, Type, Flags, _common.byreference(pcreds))
     try:
         return _advapi32.credential2dict(_common.dereference(pcreds))
     finally:
         _advapi32._CredFree(pcreds)
 
 
-def CredDelete(TargetName, Type):
-    """
-    Remove the given target name from the stored credentials.
-
-    Mimic pywin32 win32cred.CreadDelete.
+def CredDelete(TargetName, Type, Flags=0):
+    """ Remove the given target name from the stored credentials.
 
     Parameters
     ----------
-    TargetName: str-like
+    TargetName : unicode
         The target name to fetch from the keyring.
+    Type : int
+        One of the CRED_TYPE_* constants.
+    Flags : int
+        Reserved, always use 0.
+
     """
     if not Type == CRED_TYPE_GENERIC:
         raise ValueError("Type != CRED_TYPE_GENERIC not yet supported.")
-    with pywin32error():
+    with _pywin32error():
         _advapi32._CredDelete(TargetName, Type, 0)
