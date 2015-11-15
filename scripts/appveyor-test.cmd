9189b218ba64cf4eb61152f09bf94a9b965da50b
Ioannis Tziakos <ioannist@enthought.com>
2015-11-15 18:03:02 +0100

    rework the cffi pip install

--- scripts/appveyor-test.cmd
+++ scripts/appveyor-test.cmd
@@ -3,7 +3,7 @@ copy .coveragerc testrun
 cd testrun
 coverage run -m unittest discover -v win32ctypes
 if %errorlevel% neq 0 exit /b %errorlevel%
-pip install 'cffi>=1.3.0'
+pip install --cache-dir C:/egg_cache --upgrade cffi>=1.3.0
 if %errorlevel% neq 0 exit /b %errorlevel%
 coverage run -m unittest discover -v win32ctypes
 if %errorlevel% neq 0 exit /b %errorlevel%
