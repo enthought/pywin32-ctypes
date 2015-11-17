eb0ffd79da647e126b230b6e6836ff6242f3a6f0
Ioannis Tziakos <ioannist@enthought.com>
2015-11-17 21:04:47 +0000

    add echo messages

--- scripts/appveyor-test.cmd
+++ scripts/appveyor-test.cmd
@@ -1,8 +1,12 @@
+@echo off
+
 mkdir testrun
 copy .coveragerc testrun
 cd testrun
+echo. Test using ctypes
 coverage run -m unittest discover -v win32ctypes
 if %errorlevel% neq 0 exit /b %errorlevel%
 pip install --cache-dir C:/egg_cache --upgrade cffi>=1.3.0
 if %errorlevel% neq 0 exit /b %errorlevel%
+echo. Test using cffi
 coverage run -a -m unittest discover -v win32ctypes
