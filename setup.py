50b1afe648d7b214220dbb733d899a28cf73b6d0
Ioannis Tziakos <ioannist@enthought.com>
2015-11-26 00:28:49 +0000

    common place for the package version

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
+import os
 from setuptools import setup, find_packages
 
+version = open(
+    os.path.join(
+        os.path.dirname(__file__),
+        'win32ctypes', 'VERSION')).read().strip()
 setup(
     name='pywin32-ctypes',
-    version='0.0.2.dev1',
+    version=version,
     author='Enthought Inc',
     author_email='info@enthought.com',
     packages=find_packages(),
