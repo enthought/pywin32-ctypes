8f0f2afd71a54387bcb72f505789654578724cf6
Ioannis Tziakos <ioannist@enthought.com>
2015-11-26 20:32:49 +0000

    build cffi from sources

--- scripts/appveyor-test.cmd
+++ scripts/appveyor-test.cmd
@@ -8,7 +8,7 @@ cd testrun
 echo.Test using ctypes
 coverage run -m unittest discover -v win32ctypes
 if %errorlevel% neq 0 exit /b %errorlevel%
-pip install --cache-dir C:/egg_cache --upgrade cffi==1.3.0
+pip install --upgrade --no-binary cffi==1.3.0
 if %errorlevel% neq 0 exit /b %errorlevel%
 echo.Test using cffi
 coverage run -a -m unittest discover -v win32ctypes
