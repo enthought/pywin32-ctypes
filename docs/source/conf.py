e7a3c103e24e265c26db3adf622aff5001bf0b37
Ioannis Tziakos <ioannist@enthought.com>
2017-05-25 13:05:45 +0100

    change documentation theme to alabaster

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -124,9 +124,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-import enthought_sphinx_theme
-html_theme_path = [enthought_sphinx_theme.theme_path]
-html_theme = 'enthought'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
