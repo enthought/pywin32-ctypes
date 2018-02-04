086d83e8758ba7b704754a447c442876dc15a6b6
Ioannis Tziakos <mail@itziakos.gr>
2018-02-04 12:14:14 +0000

    use a meta_path hook to redirect imports to the right backend

--- win32ctypes/core/__init__.py
+++ win32ctypes/core/__init__.py
@@ -1,5 +1,5 @@
 #
-# (C) Copyright 2014 Enthought, Inc., Austin, TX
+# (C) Copyright 2014-2018 Enthought, Inc., Austin, TX
 # All right reserved.
 #
 # This file is open source software distributed according to the terms in
@@ -7,16 +7,45 @@
 #
 from __future__ import absolute_import
 
-__all__ = ['_kernel32', '_advapi32', '_winerrors', '_common']
+import sys
+import importlib
 
-from . import _winerrors
+from . import _winerrors  # noqa
 
 try:
     import cffi
 except ImportError:
-    from .ctypes import _advapi32, _common, _kernel32
     _backend = 'ctypes'
 else:
-    from .cffi import _advapi32, _common, _kernel32
     del cffi
     _backend = 'cffi'
+
+
+class BackendFinder(object):
+
+    def __init__(self, modules):
+        self.redirected_modules = [
+            'win32ctypes.core.{}'.format(module)
+            for module in modules]
+
+    def find_module(self, fullname, path=None):
+        if fullname in self.redirected_modules:
+            return self
+        else:
+            return None
+
+    def load_module(self, fullname):
+        module_name = fullname.split('.')[-1]
+        if _backend == 'ctypes':
+            new_fullname = 'win32ctypes.core.ctypes.{}'
+        else:
+            new_fullname = 'win32ctypes.core.cffi.{}'
+        module = importlib.import_module(
+            new_fullname.format(module_name))
+        return module
+
+
+sys.meta_path.append(BackendFinder([
+    '_dll', '_authentication', '_time',
+    '_common', '_resource', '_nl_support',
+    '_system_information']))
