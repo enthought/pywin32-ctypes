be42cbb26950d8e4cc29ffd3168c3b44478ac18d
Ioannis Tziakos <itziakos@gmail.com>
2023-06-19 10:58:22 +0100

    0.2.1 release (#121)
    * Set version to 0.2.1
    
    * Update changelog
    
    * Update changelog
    
    * Use setup.cfg in the build setup

--- setup.py
+++ setup.py
@@ -1,30 +1,8 @@
-import os
-from setuptools import setup, find_packages
+from pathlib import Path
+from setuptools import setup
 
+HERE = Path(__file__).parent
+version = (HERE / 'VERSION').read_text().strip()
+filename = (HERE / 'win32ctypes' / 'version.py').write_text(f'__version__ = "{version}"\n')
 
-HERE = os.path.dirname(__file__)
-version = open(
-    os.path.join(HERE, 'VERSION')).read().strip()
-
-filename = os.path.join(HERE, 'win32ctypes', 'version.py')
-with open(filename, 'w') as handle:
-    handle.write(f'__version__ = "{version}"\n')
-
-setup(
-    name='pywin32-ctypes',
-    version=version,
-    url='https://github.com/enthought/pywin32-ctypes',
-    author='Enthought Inc',
-    author_email='info@enthought.com',
-    packages=find_packages(),
-    license="BSD",
-    classifiers=[
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-    ],
-    zip_safe=False,
-    test_suite='win32ctypes.tests')
+setup()
