f93c6d974f38d31874eb39ff5ead75c9d8281c57
Ioannis Tziakos <mail@itziakos.gr>
2024-03-09 11:59:40 +0000

    Another draft attempt to support CREDENTIAL_ATTRIBUTES with ctypes

--- win32ctypes/core/ctypes/_authentication.py
+++ win32ctypes/core/ctypes/_authentication.py
@@ -1,82 +1,137 @@
 #
-# (C) Copyright 2014-18 Enthought, Inc., Austin, TX
+# (C) Copyright 2014-2024 Enthought, Inc., Austin, TX
 # All right reserved.
 #
 # This file is open source software distributed according to the terms in
 # LICENSE.txt
 #
 import ctypes
-from ctypes import POINTER, Structure, c_void_p, c_wchar_p, c_char_p, cast
+from ctypes import POINTER, Structure, c_char, c_char_p, cast, c_wchar_p
 from ctypes.wintypes import (
-    BOOL, DWORD, FILETIME, LPCWSTR)
+    BOOL, DWORD, FILETIME, LPCWSTR, LPWSTR)
 
-from win32ctypes.core.compat import is_text
-from ._common import LPBYTE, _PyBytes_FromStringAndSize
+from ._common import LPBYTE, _PyBytes_FromStringAndSize, dereference
 from ._util import function_factory, check_false_factory, dlls
-from ._nl_support import _GetACP
+
+
+class CREDENTIAL_ATTRIBUTE(Structure):
+    _fields_ = [
+        ('Keyword', LPWSTR),
+        ('Flags', DWORD),
+        ('ValueSize', DWORD),
+        ('Value', LPBYTE)]
+
+    @classmethod
+    def fromdict(cls, attribute):
+        c_attribute = cls()
+        c_pattribute = PCREDENTIAL_ATTRIBUTE(c_attribute)
+        ctypes.memset(c_pattribute, 0, ctypes.sizeof(c_attribute))
+        c_attribute.populate(attribute)
+        return c_attribute
+
+    def populate(self, attribute):
+        # Transfer values
+        self.Keyword = attribute['Keyword']
+        self.Flags = attribute.get('Flags', 0)
+        value = attribute['Value']
+        blob = c_char_p(value)
+        self.Value = cast(blob, LPBYTE)
+        self.ValueSize = len(value)
+
+
+PCREDENTIAL_ATTRIBUTE = POINTER(CREDENTIAL_ATTRIBUTE)
 
 
 class CREDENTIAL(Structure):
     _fields_ = [
-        ("Flags", DWORD),
-        ("Type", DWORD),
-        ("TargetName", c_wchar_p),
-        ("Comment", c_wchar_p),
-        ("LastWritten", FILETIME),
-        ("CredentialBlobSize", DWORD),
-        ("CredentialBlob", LPBYTE),
-        ("Persist", DWORD),
-        ("AttributeCount", DWORD),
-        ("Attribute", c_void_p),
-        ("TargetAlias", c_wchar_p),
-        ("UserName", c_wchar_p)]
+        ('Flags', DWORD),
+        ('Type', DWORD),
+        ('TargetName', LPWSTR),
+        ('Comment', LPWSTR),
+        ('LastWritten', FILETIME),
+        ('CredentialBlobSize', DWORD),
+        ('CredentialBlob', LPBYTE),
+        ('Persist', DWORD),
+        ('AttributeCount', DWORD),
+        ('Attributes', PCREDENTIAL_ATTRIBUTE),
+        ('TargetAlias', LPWSTR),
+        ('UserName', LPWSTR)]
 
     @classmethod
     def fromdict(cls, credential):
-        c_creds = cls()
-        c_pcreds = PCREDENTIAL(c_creds)
-
-        # zero-out memory
-        ctypes.memset(c_pcreds, 0, ctypes.sizeof(c_creds))
-
+        c_credentials = cls()
+        c_pcredentials = PCREDENTIAL(c_credentials)
+        ctypes.memset(c_pcredentials, 0, ctypes.sizeof(c_credentials))
         for key in credential:
             if key == 'CredentialBlob':
                 blob_data, blob_size = _make_blob(credential['CredentialBlob'])
-                c_creds.CredentialBlob = ctypes.cast(blob_data, LPBYTE)
-                c_creds.CredentialBlobSize = blob_size
+                c_credentials.CredentialBlob = blob_data
+                c_credentials.CredentialBlobSize = blob_size
+            elif key == 'Attributes':
+                attributes = credential.get('Attributes', '')
+                count = len(attributes)
+                if count == 0:
+                    continue
+                data = (PCREDENTIAL_ATTRIBUTE * count)()
+                for index in range(count):
+                    attribute = CREDENTIAL_ATTRIBUTE.fromdict(attributes[index])
+                    data[index] = PCREDENTIAL_ATTRIBUTE(attribute)
+                c_credentials.Attributes = cast(data, PCREDENTIAL_ATTRIBUTE)
+                c_credentials.AttributeCount = count
             else:
-                setattr(c_creds, key, credential[key])
-        return c_creds
+                setattr(c_credentials, key, credential[key])
+        return c_credentials
 
-
+_data = []
 PCREDENTIAL = POINTER(CREDENTIAL)
 PPCREDENTIAL = POINTER(PCREDENTIAL)
 PPPCREDENTIAL = POINTER(PPCREDENTIAL)
 
 
-def credential2dict(creds):
+def credential_attribute2dict(c_attribute):
+    attribute = {}
+    attribute['Keyword'] = c_attribute.Keyword
+    attribute['Flags'] = c_attribute.Flags
+    size = c_attribute.ValueSize
+    if size > 0:
+        value = _PyBytes_FromStringAndSize(
+            cast(c_attribute.Value, c_char_p), size)
+        attribute['Value'] = value
+    return attribute
+
+
+def credential2dict(c_credential):
     credential = {}
+
     for key, type_ in CREDENTIAL._fields_:
-        if key == u'CredentialBlob':
+        if key == 'CredentialBlob':
             blob = _PyBytes_FromStringAndSize(
-                cast(creds.CredentialBlob, c_char_p),
-                creds.CredentialBlobSize)
-            credential[u'CredentialBlob'] = blob
+                cast(c_credential.CredentialBlob, c_char_p),
+                c_credential.CredentialBlobSize)
+            credential['CredentialBlob'] = blob
+        elif key == 'Attributes':
+            attributes = []
+            count = c_credential.AttributeCount
+            data = c_credential.Attributes
+            for index in range(count):
+                attribute = credential_attribute2dict(data[index])
+                attributes.append(attribute)
+            credential['Attributes'] = tuple(attributes)
         else:
-            credential[key] = getattr(creds, key)
+            credential[key] = getattr(c_credential, key)
     return credential
 
 
 def _make_blob(data):
-    """ Convert a string to credential compatible blob dict values
+    ''' Convert a string to LPBYTE compatible blob dict values
 
-    """
+    '''
     blob_data = ctypes.create_unicode_buffer(data)
     # Create_unicode_buffer adds a NULL at the end of the
     # string we do not want that.
     blob_size = (
         ctypes.sizeof(blob_data) - ctypes.sizeof(ctypes.c_wchar))
-    blob_pointer = ctypes.cast(blob_data, LPBYTE)
+    blob_pointer = cast(blob_data, LPBYTE)
     return blob_pointer, blob_size
 
 
@@ -84,24 +139,24 @@ _CredWrite = function_factory(
     dlls.advapi32.CredWriteW,
     [PCREDENTIAL, DWORD],
     BOOL,
-    check_false_factory("CredWrite"))
+    check_false_factory('CredWrite'))
 
 _CredRead = function_factory(
     dlls.advapi32.CredReadW,
     [LPCWSTR, DWORD, DWORD, PPCREDENTIAL],
     BOOL,
-    check_false_factory("CredRead"))
+    check_false_factory('CredRead'))
 
 _CredDelete = function_factory(
     dlls.advapi32.CredDeleteW,
     [LPCWSTR, DWORD, DWORD],
     BOOL,
-    check_false_factory("CredDelete"))
+    check_false_factory('CredDelete'))
 
 _CredEnumerate = function_factory(
     dlls.advapi32.CredEnumerateW,
     [LPCWSTR, DWORD, POINTER(DWORD), PPPCREDENTIAL],
     BOOL,
-    check_false_factory("CredEnumerate"))
+    check_false_factory('CredEnumerate'))
 
 _CredFree = function_factory(dlls.advapi32.CredFree, [PCREDENTIAL])
