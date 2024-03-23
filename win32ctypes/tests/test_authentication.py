c1b9ad5acc7be54aef18d1f974e22b7a6a4a5daf
Ioannis Tziakos <mail@itziakos.gr>
2024-03-23 11:48:11 +0000

    Update tests for the authentication module

--- win32ctypes/tests/test_authentication.py
+++ win32ctypes/tests/test_authentication.py
@@ -7,18 +7,18 @@
 #
 import unittest
 
+from win32ctypes.core import _backend
 from win32ctypes.core._authentication import (
     CREDENTIAL, CREDENTIAL_ATTRIBUTE, FILETIME,
     credential2dict, credential_attribute2dict)
 from win32ctypes.constants import (
     CRED_TYPE_GENERIC, CRED_PERSIST_ENTERPRISE)
 
-
 class TestCREDENTIAL(unittest.TestCase):
 
     maxDiff = None
 
-    def test_from_dict(self):
+    def test_fromdict(self):
         # given
         username = 'john'
         password = 'doefsajfsakfj'
@@ -37,15 +37,20 @@ class TestCREDENTIAL(unittest.TestCase):
 
         # then
         self.assertEqual(result.Type, CRED_TYPE_GENERIC)
-        self.assertEqual(result.TargetName, target)
-        self.assertIsNone(result.TargetAlias)
         self.assertEqual(result.Flags, 0)
         self.assertEqual(result.AttributeCount, 0)
-        self.assertIsNotNone(result.CredentialBlob)
-        self.assertEqual(result.UserName, username)
         self.assertEqual(result.CredentialBlobSize, 26)
-        self.assertEqual(result.Comment, comment)
         self.assertEqual(result.Persist, CRED_PERSIST_ENTERPRISE)
+        if _backend == 'cffi':
+            from win32ctypes.core.cffi._util import ffi
+            self.assertEqual(ffi.string(result.UserName), username)
+            self.assertEqual(result.TargetAlias, ffi.NULL)
+            self.assertEqual(ffi.string(result.Comment), comment)
+            self.assertNotEqual(result.CredentialBlob, ffi.NULL)
+        else:
+            self.assertEqual(result.Comment, comment)
+            self.assertEqual(result.UserName, username)
+            self.assertIsNone(result.TargetAlias)
 
     def test_roundtrip(self):
         # given
@@ -54,9 +59,11 @@ class TestCREDENTIAL(unittest.TestCase):
         comment = 'Created by MiniPyWin32Cred test suite'
         target = '{0}@{1}'.format(username, password)
         keyword = 'mysecret-attribute'
-        value = b'Created by MiniPyWin32Cred test suite'
+        value = 'Created by MiniPyWin32Cred test suite'
         attribute1 = {'Keyword': keyword, 'Value': value, 'Flags': 2}
         data = {
+            'Flags': 2,
+            'TargetAlias': 'test',
             'Type': CRED_TYPE_GENERIC,
             'TargetName': target,
             'UserName': username,
@@ -70,23 +77,21 @@ class TestCREDENTIAL(unittest.TestCase):
         result = credential2dict(credential)
 
         # then
-        data['TargetAlias'] = None
-        data['Flags'] = 0
-        self.assertIsInstance(result['LastWritten'], FILETIME)
-        # Remove unused keys
+        if _backend == 'ctypes':
+            self.assertIsInstance(result['LastWritten'], FILETIME)
         del result['LastWritten']
-        del result['CredentialBlobSize']
-        del result['AttributeCount']
         result['CredentialBlob'] = result['CredentialBlob'].decode('utf-16')
+        attribute = result['Attributes'][0]
+        attribute['Value'] = attribute['Value'].decode('utf-16')
         self.assertEqual(result, data)
 
 
 class TestCREDENTIAL_ATTRIBUTE(unittest.TestCase):
 
-    def test_from_dict(self):
+    def test_fromdict(self):
         # given
         keyword = 'mysecret-attribute'
-        value = b'Created by MiniPyWin32Cred test suite'
+        value = 'Created by MiniPyWin32Cred test suite'
         data = {
             'Keyword': keyword,
             'Flags': 2,
@@ -96,12 +101,34 @@ class TestCREDENTIAL_ATTRIBUTE(unittest.TestCase):
         result = CREDENTIAL_ATTRIBUTE.fromdict(data)
 
         # then
-        self.assertEqual(result.Keyword, 'mysecret-attribute')
         self.assertEqual(result.Flags, 2)
         self.assertIsNotNone(result.Value)
-        self.assertEqual(result.ValueSize, 37)
+        self.assertEqual(result.ValueSize, 74)
+        if _backend == 'cffi':
+            from win32ctypes.core.cffi._util import ffi
+            self.assertEqual(
+                ffi.string(result.Keyword), 'mysecret-attribute')
+        else:
+            self.assertEqual(result.Keyword, 'mysecret-attribute')
 
-    def test_roundtrip(self):
+    def test_roundtrip_with_string(self):
+        # given
+        keyword = 'mysecret-attribute'
+        value = 'Created by MiniPyWin32Cred test suite'
+        data = {
+            'Keyword': keyword,
+            'Flags': 7,
+            'Value': value}
+
+        # when
+        attribute = CREDENTIAL_ATTRIBUTE.fromdict(data)
+        result = credential_attribute2dict(attribute)
+
+        # then
+        result['Value'] = result['Value'].decode('utf-16')
+        self.assertEqual(result, data)
+
+    def test_roundtrip_with_bytes(self):
         # given
         keyword = 'mysecret-attribute'
         value = b'Created by MiniPyWin32Cred test suite'
@@ -115,6 +142,4 @@ class TestCREDENTIAL_ATTRIBUTE(unittest.TestCase):
         result = credential_attribute2dict(attribute)
 
         # then
-        # Add missing keys to original data
-        result['Value'] = result['Value']
         self.assertEqual(result, data)
