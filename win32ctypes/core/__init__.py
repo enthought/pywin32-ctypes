8ef6b93f2384398b2eb6426bb979464636fb5951
Kyle Altendorf <sda@fstab.net>
2024-06-04 20:43:41 -0400

    Update __init__.py

--- win32ctypes/core/__init__.py
+++ win32ctypes/core/__init__.py
@@ -21,24 +21,17 @@ else:
     _backend = 'cffi'
 
 
-# class BackendLoader(Loader):
+class BackendLoader(Loader):
 
-#     def __init__(self, redirect_module):
-#         self.redirect_module = redirect_module
+    def __init__(self, redirect_module):
+        self.redirect_module = redirect_module
 
-#     def create_module(self, spec):
-#         return importlib.import_module(self.redirect_module)
-        
-#         importlib.util.module_from_spec(spec)
-#         return None
-
-#     def exec_module(self, module):
-        
-
-#     def load_module(self, fullname):
-#         module = importlib.import_module(self.redirect_module)
-#         sys.modules[fullname] = module
-#         return module
+    def create_module(self, spec):
+        # TODO: this seems wrong, but let's see the behavior
+        return importlib.import_module(self.redirect_module)
+ 
+    def exec_module(self, module):
+        pass
 
 
 class BackendFinder(MetaPathFinder):
@@ -55,11 +48,11 @@ class BackendFinder(MetaPathFinder):
                 redirected = f'win32ctypes.core.ctypes.{module_name}'
             else:
                 redirected = f'win32ctypes.core.cffi.{module_name}'
-            spec = importlib.util.find_spec(redirected)
-            spec.name = module_name
-            return spec
-            # loader = BackendLoader(redirected)
-            # return importlib.machinery.ModuleSpec(module_name, loader)
+            # spec = importlib.util.find_spec(redirected)
+            # spec.name = module_name
+            # return spec
+            loader = BackendLoader(redirected)
+            return importlib.machinery.ModuleSpec(module_name, loader)
         else:
             return None
 
