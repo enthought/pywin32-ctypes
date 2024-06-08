1a104038d15303124cd78697b5de3fc08eef02cf
Ioannis Tziakos <mail@itziakos.gr>
2024-06-08 12:32:35 +0100

    Augment tests for the backend selection logic

--- win32ctypes/tests/test_backends.py
+++ win32ctypes/tests/test_backends.py
@@ -23,6 +23,8 @@ class TestBackends(unittest.TestCase):
         for name in _modules:
             module = importlib.import_module(f'win32ctypes.core.{name}')
             self.assertTrue(module.__file__.endswith(f'cffi\\{name}.py'))
+            self.assertEqual(
+                module.__spec__.name, f'win32ctypes.core.cffi.{name}')
 
     @unittest.skipIf(_backend != 'ctypes', 'ctypes backend not enabled')
     def test_backend_ctypes_load(self):
@@ -30,3 +32,5 @@ class TestBackends(unittest.TestCase):
         for name in _modules:
             module = importlib.import_module(f'win32ctypes.core.{name}')
             self.assertTrue(module.__file__.endswith(f'ctypes\\{name}.py'))
+            self.assertEqual(
+                module.__spec__.name, f'win32ctypes.core.ctypes.{name}')
