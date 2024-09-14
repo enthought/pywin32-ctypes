9377d303eac6a23d1c3cca311cd5b05224f5e4e8
Ioannis Tziakos <mail@itziakos.gr>
2024-09-14 12:00:51 +0100

    We do not need systemtime

--- win32ctypes/core/cffi/_winbase.py
+++ win32ctypes/core/cffi/_winbase.py
@@ -15,18 +15,6 @@ typedef struct _FILETIME {
   DWORD dwHighDateTime;
 } FILETIME, *PFILETIME, *LPFILETIME;
 
-
-typedef struct _SYSTEMTIME {
-  WORD wYear;
-  WORD wMonth;
-  WORD wDayOfWeek;
-  WORD wDay;
-  WORD wHour;
-  WORD wMinute;
-  WORD wSecond;
-  WORD wMilliseconds;
-} SYSTEMTIME, *PSYSTEMTIME, *LPSYSTEMTIME;
-
 """)
 
 
@@ -43,36 +31,9 @@ class _FILETIME(object):
         c_filetime.dwHighDateTime = filetime['dwHighDateTime']
         return c_filetime
 
-
-class _SYSTEMTIME(object):
-
-    def __call__(self):
-        return ffi.new("PSYSTEMTIME")[0]
-
-    @classmethod
-    def fromdict(cls, systemtime):
-        factory = cls()
-        c_systemtime = factory()
-        c_systemtime.wYear = systemtime['Year']
-        c_systemtime.wMonth = systemtime['Month']
-        c_systemtime.wDayOfWeek = systemtime['DayOfWeek']
-        c_systemtime.wDay = systemtime['Day']
-        c_systemtime.wHour = systemtime['Hour']
-        c_systemtime.wMinute = systemtime['Minute']
-        c_systemtime.wSecond = systemtime['Second']
-        c_systemtime.wMilliseconds = systemtime['Milliseconds']
-        return c_systemtime
-
-
 def PFILETIME(value=None):
     return ffi.new("PFILETIME", ffi.NULL if value is None else value)
 
 
-def PSYSTEMTIME(value=None):
-    return ffi.new("PSYSTEMTIME", ffi.NULL if value is None else value)
-
-
 FILETIME = _FILETIME()
-SYSTEMTIME = _SYSTEMTIME()
 LPFILETIME = PFILETIME
-LPSYSTEMTIME= PSYSTEMTIME
